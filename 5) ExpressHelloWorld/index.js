f76bb41b733c1f28f67f2566db647714438ea4e0
Khaldoun Halawani <kldonnn@gmail.com>
2023-02-18 13:51:52 +0200

    Delete by ID, batch Create

--- 5) ExpressHelloWorld/index.js
+++ 5) ExpressHelloWorld/index.js
@@ -17,19 +17,38 @@ app.get('/users', (req, res) => {
   res.send(USERS);
 });
 
-app.delete('/users', (req, res) => {
-  USERS.pop();
-  res.send("One user deleted!")
-});
-
 app.post('/users', (req, res) => {
-  console.log(req.body);
   USERS.push({
     id: req.body.id || faker.random.numeric(3),
     name: req.body.name || faker.name.fullName(),
     dob: req.body.dob || faker.date.birthdate()
   });
   res.send("User Added!");
+});
+
+app.post('/users/batch', (req, res) => {
+  const count = Number(req.query.count) || 1;
+
+  for (let i = 0; i < count; i++) {
+    USERS.push({
+      id: req.body.id || faker.random.numeric(3),
+      name: req.body.name || faker.name.fullName(),
+      dob: req.body.dob || faker.date.birthdate()
+    });
+  }
+  res.send(`${count} User/s Added!`);
+});
+
+// app.delete('/users/:id/:name', (req, res) => {  // you can send multiple params
+app.delete('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const userIndex = USERS.findIndex(item => item.id === id);
+  if (userIndex >= 0) {
+    USERS.splice(userIndex, 1);
+    res.send("One user deleted!")
+  } else {
+    res.send("User Not found!")
+  }
 });
 
 app.get('/about-us', (req, res) => {
