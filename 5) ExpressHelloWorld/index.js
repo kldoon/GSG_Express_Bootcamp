d65ca20c828933f274f0120ff23ead50e7888e7d
Khaldoun Halawani <kldonnn@gmail.com>
2023-02-18 12:48:03 +0200

    Add delete route

--- 5) ExpressHelloWorld/index.js
+++ 5) ExpressHelloWorld/index.js
@@ -1,11 +1,38 @@
 import express from 'express';
+import { faker } from '@faker-js/faker';
+
 
 const app = express();
 const port = 3000;
-const host = '10.10.112.57'
+const host = '127.0.0.1';
+
+const USERS = [];
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  res.send(USERS);
+});
+
+app.delete('/users', (req, res) => {
+  USERS.pop();
+  res.send("One user deleted!")
+});
+
+app.get('/users/add', (req, res) => {
+  USERS.push({
+    id: faker.datatype.uuid(),
+    fullName: faker.name.fullName(),
+    dob: faker.date.birthdate()
+  });
+
+  res.send("User Added!");
+});
+
+app.get('/about-us', (req, res) => {
+  res.send('We are a team of developers who learns ExpressJS!');
 });
 
 app.listen(port, host, () => {
