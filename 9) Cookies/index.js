4624b3cfff82c5860dbe418366692cf8d1218053
Khaldoun Halawani <kldonnn@gmail.com>
2023-02-24 15:08:33 +0200

    Login Cookies Example

--- 9) Cookies/index.js
+++ 9) Cookies/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  credentials: true
+}));
 
 const port = 3000;
 const host = '127.0.0.1';
@@ -17,6 +23,26 @@ app.get('/', (req, res) => {
     });
   }
   res.send('Hello World!');
+});
+
+
+app.get('/secretData', (req, res) => {
+  const authToken = req.cookies['authToken'];
+  if (authToken && authToken === 'lkjlkjhUIYOLJKH(*^&(U') {
+    res.send('Very Secret Data!');
+  } else {
+    res.status(403).send('You are not authorized, GO HOME!');
+  }
+});
+
+app.post('/login', (req, res) => {
+  if (req.body.username === 'ahmad' && req.body.password === '123') {
+    res.cookie('username', req.body.username);
+    res.cookie('authToken', 'lkjlkjhUIYOLJKH(*^&(U');
+    res.status(200).send('Welcome ' + req.body.username);
+  } else {
+    res.status(401).send('Wrong username Or Password!');
+  }
 });
 
 app.listen(port, host, () => {
