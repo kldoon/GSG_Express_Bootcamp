e192faf193ffa720155e4ad7af5800c41ae2d5d7
Khaldoun Halawani <kldonnn@gmail.com>
2023-02-25 12:27:01 +0200

    Add Student controller, with create function

---
+++ 11) Sequelize/controllers/student.controller.js
@@ -0,0 +1,27 @@
+import db from "../models/index.js";
+
+const create = (req, res) => {
+  const std = {
+    id: 1,
+    name: 'Ahmad',
+    gpa: 2.5,
+    is_graduated: false
+  };
+
+  db.Student.create(std)
+    .then(data => {
+      res.status(201).send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err.message || "Something went wrong");
+    });
+};
+
+const findAll = (req, res) => {
+
+};
+
+export default {
+  create,
+  findAll
+}
