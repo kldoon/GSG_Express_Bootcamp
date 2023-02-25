9a14f469999a4806d83b5a77133ca82f4ae0d2b0
Khaldoun Halawani <kldonnn@gmail.com>
2023-02-25 11:49:47 +0200

    Sequelize DB Sync

--- 11) Sequelize/index.js
+++ 11) Sequelize/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import db from './models/index.js';
 
 const PORT = 3000;
 const HOST = '127.0.0.1';
@@ -6,6 +7,13 @@ const app = express();
 
 app.use(express.json());
 
+db.sequelize.sync()
+  .then(() => {
+    console.log("DB Sync Done Successfully!")
+  })
+  .catch((err) => {
+    console.log(`Failed to Sync with DB: ${err.message}`);
+  });
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is listening on http://${HOST}:${PORT}`)
